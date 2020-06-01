f164055e7a05177d16b22462c6ae5af1071f64f5
Ubuntu <ubuntu@ubuntu.iHoser.net>
2020-06-01 22:38:40 +0000

    Use pigpio daemon and add listener class

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,37 +1,53 @@
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
-#include <pigpio.h>
+#include <pigpiod_if2.h>
 
-// Toggle LEDs to the message's data
-// LED Pins are pin 0 and 7 (wiringPi)
+// toggle LEDs to the message's data
+// LED pins are pin 4 and 17 (BCM)
 const int LED_BLUE = 4;
 const int LED_YELLOW = 17;
 
-void chatterCallback(const std_msgs::Bool msg)
+// a class to associate the callback function to a pigpio daemon 
+class ListenerGPIO
 {
-    gpioWrite(LED_BLUE, msg.data);
-    gpioWrite(LED_YELLOW, !msg.data);
+    public:
+        ListenerGPIO(int pi) : pigpio_id(pi) {}
 
-    ROS_INFO("I heard: [%d]. Toggling LEDs.", msg.data);
-}
+        void chatterCallback(const std_msgs::Bool msg)
+        {
+            gpio_write(pigpio_id, LED_BLUE, msg.data);
+            gpio_write(pigpio_id, LED_YELLOW, !msg.data);
+
+            ROS_INFO("I heard: [%d]. Toggling LEDs.", msg.data);
+        }
+
+    private:
+        const int pigpio_id;
+};
 
 int main(int argc, char **argv)
 {
-    // configure gpio ports for output
-    if (gpioInitialise() < 0)
+    // connect to the pigpio daemon and configure gpio ports
+    int pi = pigpio_start(0, 0);
+    if (pi < 0)
     {
         ROS_INFO("pigpio failed to initialise");
         return 1;
     }
-    gpioSetMode(LED_BLUE, PI_OUTPUT);
-    gpioSetMode(LED_YELLOW, PI_OUTPUT);
+    set_mode(pi, LED_BLUE, PI_OUTPUT);
+    set_mode(pi, LED_YELLOW, PI_OUTPUT);
 
     // configure ROS
     ros::init(argc, argv, "listener");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
-    ros::spin();
 
-    gpioTerminate();
+    // create listener object and subscribe it to the "chatter" topic
+    ListenerGPIO listener(pi);
+    ros::Subscriber sub = nh.subscribe("chatter", 1000, &ListenerGPIO::chatterCallback, &listener);
+    
+    ros::spin();
+    
+    // disconnect from the daemon
+    pigpio_stop(pi);
     return 0;
 }
